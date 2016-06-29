5ab474f404a7d28e924432b3e6a2eb5bf7e94f0e
Arka Adhya <aadhya@onesourcevirtual.com>
2016-06-29 23:30:03 +0000

    Initialize Repo

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT;
+
+
+// app.get('/', function(req, res){
+//     res.send('Hello Express');  
+// });
+
+var middleWare = {
+    requireAuthentication: function(req, res, next){
+        console.log('Private route hit');
+        next();
+    },
+    logger: function (req, res, next){
+        var date = Date().toString();
+        console.log('Request: ' + date + ' ' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+}
+
+app.use(middleWare.logger);
+
+app.get('/about', middleWare.requireAuthentication, function(req, res){
+    res.send('About Us!');  
+});
+
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname);
+
+//Not reserved
+app.listen(PORT, process.env.IP, function(){
+  console.log("Express server started on port " + PORT + '!');
+ });
